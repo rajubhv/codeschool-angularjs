109c6986ad2651bbcb74635f9beb447831aa1198
Venkateswaraju Bhupathiraju <Venkateswaraju.Bhupathiraju@avon.com>
2015-05-17 18:21:56 -0400

    ng-show, ng-hide

--- js/app.js
+++ js/app.js
@@ -3,11 +3,31 @@
     
     app.controller('StoreController',function (){
         this.product=gem;
+        this.products=gems;
     });
     
     var gem={
         name: 'Dodecahedron',
         price: 2.95,
-        description: 'This is the first gem in store'
-    }
+        description: 'This is the first gem in store',
+        canPurchase: true,
+        soldOut: false
+    };
+    
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: 'This is the first gem in store',
+            canPurchase: true,
+            soldOut: false
+        },
+        {
+            name: 'Pentagonal Gem',
+            price: 5.95,
+            description: 'This is the second gem in store',
+            canPurchase: true,
+            soldOut: false
+        }
+        ];
 })();
